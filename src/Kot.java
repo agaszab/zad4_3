3e7665728a0c625a9e9aca1957128f921829716d
agatito <agatito@yopmail.com>
2018-01-21 19:13:46 +0100

    wyświetlanie kota

---
+++ src/Kot.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Kot {
+
+    public static void main (String [] args) {
+
+        String napis;
+        Scanner odczyt = new Scanner(System.in);
+
+        napis = odczyt.nextLine();
+
+        System.out.println(napis);
+
+    }
+}
